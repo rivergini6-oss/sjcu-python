4a2ccd51c0c74ca4b1e5605509abcc02ffce75dd
강진이 <rivergini6@gmail.com>
2026-03-13 13:22:46 +0900

    다섯번째 커밋

---
+++ Python Study/test.py
@@ -0,0 +1,50 @@
+#int()정수, float()실수, str()문자열 변환,
+##%s문자열 출력,%d정수형 숫자,%f실수형 숫자출력
+variable=input()
+print("input string %s, int %d" % (variable, float(variable)))
+
+#문자열 테스트
+variable=input("안녕하세요")
+print()
+
+#연습
+variable=1234
+print(variable)
+
+variable=1.234E2
+print(variable)
+
+variable=1.234e-2
+print(variable)
+
+variable=0x1f
+print(variable)
+
+variable=3/4
+print(variable)
+
+#2에 3승
+variable=2**3
+print(variable)
+
+#나누기의 나머지값
+variable=5%2
+print(variable)
+
+#나누기의 몫
+variable=5//2
+print(variable)
+
+#나누기 값
+variable=5/2
+print(variable)
+
+
+
+
+
+
+
+
+
+
